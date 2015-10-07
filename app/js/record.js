080761be39f40edf95f1d812372dab235237619f
kwhoh1 <kwhoh1@student.monash.edu>
2015-10-07 17:11:29 +1100

    V2
    Task 1
    1. Edited the record.js for the 1st task. Still unsure about currentPath
    object. Please have a look!
    2. addLocationToCurrentPathObject function should function perfectly.
    Also have a look.
    
    Task 2(STILL IN COMMENTS)
    1. Added calculations for distance and duration.
    2. Added the code to plot route lot route

--- app/js/record.js
+++ app/js/record.js
@@ -1,94 +1,134 @@
-
 navigator.geolocation.watchPosition(updatePosition);
 
 function updatePosition(positionNew){
     if (markerPosition){
         console.log(positionNew);
-        var googlePosition = {lat: positionNew.coords.latitude, lng: positionNew.coords.longitude};
+        var googlePosition = {lat: positionNew.coords.latitude, 
+                              lng: positionNew.coords.longitude};
         markerPosition.position = googlePosition;
         
             
     }
 }
-    
-//    if (recordOn == true){
-//                addLocationToCurrentPathObject(positionNew);
-//                addLocationToCurrentMapsPolyLine(positionNew);
-//                updateCurrentInfo(positionNew);
-//    }
-    
-//function addLocationToCurrentPathObject(positionNew){
-//    var placementNew = {
-//        lat: positionNew.coords.latitude,
-//        lng: positionNew.coords.longitude,
-//        time: positionNew.timesamp
-//    }
-//    currentPath.paths[currentPath..paths.length] = placementNew;
-//}
-//
+
+    if (recordOn == true){
+                addToPathObject(positionNew);
+                addToCurrentMapsPolyLine(positionNew);
+                updateCurrentInfo(positionNew);
+    }
+
+/*
+function addToCurrentPathObject(positionNew){
+    var placementNew = {
+        lat: positionNew.coords.latitude,
+        lng: positionNew.coords.longitude,
+        time: positionNew.timesamp
+    }
+    currentPath.paths[currentPath..paths.length] = placementNew;
+}
+*/
+    function addToCurrentPathObject(positionNew) {
+        var placementNew = document.getElementById('geolocation');
+        placementNew.innerHTML = {
+            latitude:   positionNew.coords.latitude,
+            longitude:  positionNew.coords.longitude,
+            timestamp:  new Date(positionNew.timestamp)
+        }
+        currentPath.paths[currentPath.paths.length] = placementNew;
+    }    
+/*
+
+
+
 ////currentPath is an object as such:
-////    
-////    {
-////    title: “The Route Name”,
-////    paths: [{
-////        lat: value,
-////        lng: value,
-////        time: value
-////    }, {
-////        lat: value,
-////        lng: value,
-////        time: value
-////    }, {
-////        etc, etc
-////    }]
-////    }
+  /*  
+    {
+    route_name: '',
+    paths: [{
+        lat: value,
+        lng: value,
+        time: value
+    }, {
+        lat: value,
+        lng: value,
+        time: value
+    }, {
+        etc, etc
+    }]
+    }
+*/
 
 
-/* 
-//Buttons
-  <div data-role="content"> //OPTIONAL: text field for user to enter name for their workout 
-    <p id="startTracking_status"></p>
-      <div data-role="fieldcontain" class="ui-hide-label">
-        <label for="track_id">Track ID/Name:</label>
-        <input type="text" name="track_id" id="track_id" placeholder="Workout ID/Name"/>
-      </div>
+
+/*
+//Calculate distance travelled
+function gps_distance(lat1, lon1, lat2, lon2)
+{
+  // http://www.movable-type.co.uk/scripts/latlong.html
+    var R = 6371; // km
+    var dLat = (lat2-lat1) * (Math.PI / 180);
+    var dLon = (lon2-lon1) * (Math.PI / 180);
+    var lat1 = lat1 * (Math.PI / 180);
+    var lat2 = lat2 * (Math.PI / 180);
  
-    <button data-role="button" id="startTracking_start">Start Tracking</button>
-    <button data-role="button" id="startTracking_stop">Stop Tracking</button>  
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c;
      
-        <p id="startTracking_debug"></p>
-         
-  </div> 
-  
-  //Tracking/recording movement
-  var track_id = '';      // Name/ID of the exercise
-  var watch_id = null;    // ID of the geolocation
-  var tracking_data = []; // Array containing GPS position objects
+    return d;
+}
+*/
 
-    $("#startTracking_start").live('click', function(){
 
-        // Start tracking the User
-        watch_id = navigator.geolocation.watchPosition(    //polls GPS feature of the phone and recieves constant updates on location
 
-            // Success                                     //if successful, pass the currentPath object
-            function(currentPath){
-                tracking_data.push(currentPath);
-            },
+/*
+// Calculate duration 
+start_time = new Date(data[0].timestamp).getTime();
+end_time = new Date(data[data.length-1].timestamp).getTime();
+ 
+total_time_ms = end_time - start_time;
+total_time_s = total_time_ms / 1000;
+ 
+final_time_m = Math.floor(total_time_s / 1000);
+final_time_s = total_time_s - (final_time_m * 60);
+ 
+// Display total distance and time
+$("#track_info_info").html('Travelled <strong>' + total_km_rounded + '</strong> km in <strong>' + final_time_m + 'm</strong> and <strong>' + final_time_s + 's</strong>');
+*/
 
-            // Error
-            function(error){
-                console.log(Unable to track run!);
-            },
 
-            // Settings
-            { frequency: 3000, enableHighAccuracy: true });
 
-        // OPTIONAL: Tidy up the UI
-        track_id = $("#track_id").val();
+/*
+// Plotting the route
+// Set the initial Lat and Long of the Google Map
+var myLatLng = new google.maps.LatLng(data[0].coords.latitude, data[0].coords.longitude);
+ 
+// Google Map options
+var myOptions = {
+  zoom: 15,
+  center: myLatLng,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+ 
+// Create the Google Map, set options
+var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
-        $("#track_id").hide();
-
-        $("#startTracking_status").html("Tracking workout: <strong>" + track_id + "</strong>");
-    });
-  */
-  
+var trackCoords = [];
+ 
+// Add each GPS entry to an array
+for(i=0; i<data.length; i++){
+    trackCoords.push(new google.maps.LatLng(data[i].coords.latitude, data[i].coords.longitude));
+}
+ 
+// Plot the GPS entries as a line on the Google Map
+var trackPath = new google.maps.Polyline({
+  path: trackCoords,
+  strokeColor: "#FF0000",
+  strokeOpacity: 1.0,
+  strokeWeight: 2
+});
+ 
+// Apply the line to the map
+trackPath.setMap(map);
+*/
