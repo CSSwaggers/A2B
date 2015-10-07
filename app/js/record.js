9ca9e35dd71a4aa12e0582a5f911c920f62e5e4d
George Millington <gomil1@student.monash.edu>
2015-10-07 17:51:45 +1100

    Testing recording paths

--- app/js/record.js
+++ app/js/record.js
@@ -1,30 +1,60 @@
+var placement;
+var recordOn = false;
+var recordWatchId;
+var pathCurrent = {
+    paths: [],
+    positionAdd: function(positionNew){ pathCurrent.paths[pathCurrent.paths.length] = {
+        lat: positionNew.coords.latitude,
+        lng: positionNew.coords.longitude,
+        time: positionNew.timestamp
+    }}
+};
+var polylineCurrent = new google.maps.Polygon({
+    map: map,
+    paths: [],
+    strokeColor: '#FF0000',
+    strokeOpacity: 1,
+    zIndex: 100
+});
+    
+navigator.geolocation.watchPosition(markerUpdatePosition);
 
-navigator.geolocation.watchPosition(updatePosition);
-
-function updatePosition(positionNew){
+function markerUpdatePosition(positionNew){
     if (markerPosition){
         console.log(positionNew);
         var googlePosition = {lat: positionNew.coords.latitude, lng: positionNew.coords.longitude};
         markerPosition.position = googlePosition;
-        
-            
     }
 }
-    
-//    if (recordOn == true){
-//                addLocationToCurrentPathObject(positionNew);
-//                addLocationToCurrentMapsPolyLine(positionNew);
-//                updateCurrentInfo(positionNew);
-//    }
-    
-//function addLocationToCurrentPathObject(positionNew){
-//    var placementNew = {
-//        lat: positionNew.coords.latitude,
-//        lng: positionNew.coords.longitude,
-//        time: positionNew.timesamp
-//    }
-//    currentPath.paths[currentPath..paths.length] = placementNew;
-//}
+
+function recordStartStop(){
+    if (recordOn == false){
+        recordWatchId = navigator.geolocation.watchPosition(recordUpdatePosition);
+        markerPosition.setAnimation(google.maps.Animation.BOUNCE);
+        recordOn = true;
+    }
+    else {
+        //navigator.geolocation.clearWatch(recordWatchId);
+        console.log('turn off watchPosition');
+        recordOn = false;
+    }
+}
+
+function recordUpdatePosition(positionNew){
+    console.log('logging new pos');
+    pathCurrent.positionAdd(positionNew);
+    polylinePathNew = polylineCurrent.getPath();
+    if (polylinePathNew){
+        polylinePathNew[polylinePathNew.length] = {lat: positionNew.coords.latitude, lng: positionNew.coords.longitude}
+        console.log('Plotting next point as'+polylinePathNew[polylinePathNew.length-1]);
+    };
+    if (!polylinePathNew){
+                polylinePathNew = [{lat: positionNew.coords.latitude, lng: positionNew.coords.longitude}];
+        console.log('Plotting first point as '+polylinePathNew[polylinePathNew.length-1].lat);
+    };
+    polylineCurrent.setPath(polylinePathNew);
+    //updateCurrentInfo(positionNew);
+}
 //
 ////currentPath is an object as such:
 ////    
